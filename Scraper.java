ecdf1980335a0b7cdc83aca7fbc1ed240119e573
unknown <David>
2018-11-13 20:30:39 +0100

    new

--- Scraper.java
+++ Scraper.java
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.*;
 
-//help
+//test1
 public class Scraper {
     public static void main(String[] args) throws Exception {
         List<String> prices;
