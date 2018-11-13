40fc9c79c4b2b5a1fcf6faa713f59ec7c8f1092b
DavidBoissevain <45010541+DavidBoissevain@users.noreply.github.com>
2018-11-13 18:53:04 +0100

    Update Scraper.java

--- Scraper.java
+++ Scraper.java
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.*;
 
+
+//test!
 public class Scraper {
     public static void main(String[] args) throws Exception {
         List<String> prices;
