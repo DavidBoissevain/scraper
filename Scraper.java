02cafd25c104eff044f53f0bddd55c0b9f4fffa5
unknown <David>
2018-11-14 21:39:13 +0100

    Better notation1

--- Scraper.java
+++ Scraper.java
@@ -4,12 +4,13 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.*;
 
-//test12
+
 public class Scraper {
     public static void main(String[] args) throws Exception {
         List<String> prices;
         List<String> reviews;
         for (int i = 819300; i<= 1000000; i++){
+            //Fetch the URL's for the product pages and the reviews of the product pages
             String URL = "https://www.coolblue.nl/en/product/";
             URL = URL + i;
             Document document = Jsoup.connect(URL).userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
@@ -20,14 +21,17 @@ public class Scraper {
             Document documentr = Jsoup.connect(URLr).userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
                     .referrer("http://www.google.com").ignoreHttpErrors(true).get();
 
+            //Fetch the price, if it is not available, skip the page
             prices = document.select("strong.sales-price--current").eachText();
             if (prices.size()>0){
+                //Print the title, price, amount of reviews and the rating.
                 reviews = document.select("a.review-rating--reviews-link").eachText();
                 String ratings = documentr.select("span.review-rating--enhanced-score").text();
                 String title = document.select("span.js-product-name").text();
                 System.out.println("<product  product_ID = "+ i + ">" );
                 System.out.println("    <title>" + title + "</title>");
                 System.out.println("    <price>" + prices.get(0) + "</price>");
+                //If there are no reviews written yet, replace it by "0 reviews" to be in line with others
                 if (reviews.get(0).equals("Write first review")){
                     System.out.println("    <ratings>" + "0 reviews" + "</ratings>");
                 }
@@ -37,38 +41,35 @@ public class Scraper {
                 }
 
                 //Code that scrapes specifications
+                System.out.println("    <specifications>");
                 Elements spec_cats = document.select("div.js-specifications-content div.grid-section-xs--gap-6");
-                boolean hasSpecs =false;
                 for (Element category: spec_cats){
                     String thisCategory = category.select("h3").text();
-                    String cat = "	<specifications category=\""+thisCategory+"\">";
-                    System.out.println(cat);
-
-
-
+                    System.out.println("         <"+thisCategory.toLowerCase().replaceAll(" ", "-" ) +"-specifications>");
                     Elements specification = category.select("div");
-                    //System.out.println(specification.text());
-                    //pk
                     for (Element  s: specification) {
-                        String spec = "         <"+s.select("dt").text()+">"+s.select("dd").text()+"</"+s.select("dt").text()+">";
+                        String spec = "               <"+deDup(s.select("dt").text()).toLowerCase()+">"+s.select("dd").text()+"</"+deDup(s.select("dt").text()).toLowerCase()+">";
                         if (!s.select("dd").text().equals("")) {
                             System.out.println(spec);
-                            hasSpecs = true;
                         }
                     }
-                    String cat2 = "	</specifications category=\""+thisCategory+"\">";
-                    System.out.println(cat2);
+                    System.out.println("         </"+thisCategory.toLowerCase().replaceAll(" ", "-" )  +"-specifications>");
                 }
-
-
-                //makes sure there is no sole </specifications>
-                if (hasSpecs){
-                    System.out.println("	</specifications>");
-                }
+                System.out.println("	</specifications>");
                 //End of specification scraping code
+
                 System.out.println("</product>");
                 System.out.println("");
             }
         }
+    }
+
+    /**
+     * Removes double words from a String
+     * @param s a string containing double words
+     * @return a String that does not contain double words anymore
+     */
+    public static String deDup(String s) {
+        return new LinkedHashSet<String>(Arrays.asList(s.split(" "))).toString().replaceAll("(^\\[|\\]$)", "").replace(", ", "-");
     }
 }
